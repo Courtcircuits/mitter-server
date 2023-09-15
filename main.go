3070fa3345e865d503a7657e4fcfc6908d102d7d
tradulescu <tristan-mihai.radulescu@etu.umontpellier.fr>
2023-09-15 14:54:30 +0200

    fix port for railway instance

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Courtcircuits/mitter-server/api"
 	"github.com/Courtcircuits/mitter-server/storage"
 )
@@ -8,7 +10,13 @@ import (
 func main() {
 	storage := storage.NewDatabase()
 
-	server := api.NewServer(":8080", *storage)
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	server := api.NewServer(":"+port, *storage)
 
 	server.Start()
 }
